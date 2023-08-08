77ee159f0448c6ee70f2a2d827a1a6c41c3ddb07
Rononoa13 <sumitluitel42@gmail.com>
2023-08-08 21:04:47 +0545

    Added feature to set the meeting

--- models.py
+++ models.py
@@ -2,10 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
-class SetMetting(db.Model):
+class SetMeeting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     room_name = db.Column(db.String(100), nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False)
-    end_time = db.Column(db.DateTime, nullable=False)
+    start_time = db.Column(db.Integer, nullable=False)
+    end_time = db.Column(db.Integer, nullable=False)
     availability = db.Column(db.Boolean, default=True)
