7f7eb6f94005643e6f3d40a46b852353a852d969
Rononoa13 <sumitluitel42@gmail.com>
2023-08-10 21:30:22 +0545

    Admin Updates

--- models.py
+++ models.py
@@ -2,6 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+
 class SetMeeting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     room_name = db.Column(db.String(100), nullable=False)
